8e3baf5c111836146534f232638fc697971c36be
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2025-05-15 19:13:05 +0530

    Chore: Adding space

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 exports.log = function (message) {
     const date = new Date();
     const formattedDate = date.toISOString().replace(/T/, ' ').replace(/\..+/, '');
-    console.log(`[${formattedDate}] ${message}`);
+    console.log(`[${formattedDate}] ${message} `);
 }
